9b3a83aef851b52c3c2e50ca7100a3f24c98f8ce
Nandakishor.V <nandakishor.v2020@vitbhopal.ac.in>
2022-09-15 23:24:57 +0530

    first commit

--- app/src/main/java/com/example/blooddonation/RecipientRegistrationActivity.java
+++ app/src/main/java/com/example/blooddonation/RecipientRegistrationActivity.java
@@ -5,23 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
-    private Button registrationButton;
+public class RecipientRegistrationActivity extends AppCompatActivity {
+    private TextView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_recipient_registration);
 
-        registrationButton = findViewById(R.id.recipientButton);
-
-        registrationButton.setOnClickListener(new View.OnClickListener() {
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,SelectRegistrationActivity.class);
+                Intent intent = new Intent(RecipientRegistrationActivity.this,LoginActivity.class);
                 startActivity(intent);
+
             }
         });
     }
